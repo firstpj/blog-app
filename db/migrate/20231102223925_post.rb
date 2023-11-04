b2a10e8eb5f0b3fda4b3006accb69571a838816e
firstpj <114886262+firstpj@users.noreply.github.com>
2023-11-04 10:08:32 +0100

    Fix linters

--- db/migrate/20231102223925_post.rb
+++ db/migrate/20231102223925_post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Migration[7.1]
   def change
-    reate_table :posts do |t|
+    create_table :posts do |t|
       t.string :title
       t.text :text
       t.integer :comments_counter
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_31_101633) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
-
+ActiveRecord::Schema[7.1].define(version: 20_231_102_224_118) do
   create_table 'comments', force: :cascade do |t|
     t.text 'text'
-    t.bigint 'user_id', null: false
-    t.bigint 'post_id', null: false
+    t.integer 'user_id', null: false
+    t.integer 'post_id', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
     t.index ['post_id'], name: 'index_comments_on_post_id'
@@ -25,8 +22,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_31_101633) do
   end
 
   create_table 'likes', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.bigint 'post_id', null: false
+    t.integer 'user_id', null: false
+    t.integer 'post_id', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
     t.index ['post_id'], name: 'index_likes_on_post_id'
@@ -38,7 +35,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_31_101633) do
     t.text 'text'
     t.integer 'comments_counter'
     t.integer 'likes_counter'
-    t.bigint 'author_id', null: false
+    t.integer 'author_id', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
     t.index ['author_id'], name: 'index_posts_on_author_id'
