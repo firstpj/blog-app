0c20a8acce405190e6f872ae4ef600c69940e911
firstpj <114886262+firstpj@users.noreply.github.com>
2023-11-06 23:48:54 +0100

    add app/views/layout/application.html.erb, add application files update

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,20 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id', counter_cache: :posts_counter
-  has_many :likes, dependent: :destroy
-  has_many :comments, dependent: :destroy
+  # Associations
+  has_many :comments
+  has_many :likes
+  belongs_to :author, class_name: 'User'
 
-  after_save :increment_user_posts_counter
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def recent_comments
-    comments.order(created_at: :desc).limit(5)
+  after_save :update_user_posts_counter
+
+  def update_user_posts_counter
+    author.increment!(:posts_counter)
   end
 
-  def increment_user_posts_counter
-    author.increment!(:posts_counter)
+  def five_most_recent_comments
+    Comment.where(post_id: id).order(created_at: :desc).limit(5)
   end
 end
